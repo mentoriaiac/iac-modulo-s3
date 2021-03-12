9be75850f27d1dccddc6257367b2518a72354c6f
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-03-12 16:19:33 -0300

    refatora modulo cria nova estrutura de arquivos com versions.tf, variables.tf e main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-variable "bucket_name" {
-  type = string
-}
-
-variable "acl" {
-  type    = string
-  default = "private"
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
   acl    = var.acl
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "acl" {
+  type    = string
+  default = "private"
+}
