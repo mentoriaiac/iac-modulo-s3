38f71af7b34b43a26f259b08ecd492395cbc8e2e
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-03-31 20:39:58 -0300

    chore: inclui condiçao para validar parametro acl

--- variables.tf
+++ variables.tf
@@ -1,8 +1,14 @@
 variable "bucket_name" {
-  type = string
+  type        = string
+  description = "Nome do bucket"
 }
 
 variable "acl" {
-  type    = string
-  default = "private"
+  type        = string
+  description = "ACL aplicada ao bucket"
+  default     = "private"
+  validation {
+    condition     = contains(["private", "public-read", "public-read-write", "aws-exec-read", "authenticated-read", "log-delivery-write"], var.acl)
+    error_message = "ACL inválida, valores permitidos: private, public-read, public-read-write, aws-exec-read, authenticated-read, log-delivery-write."
+  }
 }
