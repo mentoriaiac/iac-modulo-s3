9599919b587b0a89d921aaaff4870b1e7dff9367
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-03-31 18:42:14 -0300

    chore: inclui acl na lista de outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,16 @@
 output "this_s3_bucket_id" {
-  value = aws_s3_bucket.my_bucket.id
+  value       = aws_s3_bucket.my_bucket.id
+  description = "The id of this bucket."
 }
 
 output "this_s3_bucket_region" {
-  value = aws_s3_bucket.my_bucket.region
+  value       = aws_s3_bucket.my_bucket.region
+  description = "The aws region of this bucket"
 }
+
+output "this_s3_bucket_acl" {
+  value       = aws_s3_bucket.my_bucket.acl
+  description = "The aws region of this bucket"
+}
+
+
